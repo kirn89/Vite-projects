237df835afc574977e1e9112ca81242be28ec804
sanjay.thakur <sanjay.thakur@dghub.io>
2023-06-02 20:00:48 +0545

    fixed image url bug

--- src/CeoDogProject/Component/DogDetail.jsx
+++ src/CeoDogProject/Component/DogDetail.jsx
@@ -1,49 +1,56 @@
 import axios from "axios";
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const DogDetail = ({ data }) => {
-  // const dataArray = Object.keys(data);
-  // console.log(dataArray);
-  const [currentBreed, setCurrentBreed] = useState("");
+  const [images, setImages] = useState([]);
 
   useEffect(() => {
     const fetchImageData = async () => {
       try {
-        const response = await axios.get(
-          "https://dog.ceo/api/breeds/image/random"
-        );
-        console.log(response);
+        const imagePromises = data.map(async (breed) => {
+          const response = await axios.get(
+            `https://dog.ceo/api/breed/${breed}/images/random`
+          );
+          return response.data.message;
+        });
+
+        const imageData = await Promise.all(imagePromises);
+        setImages(imageData);
       } catch (error) {
         console.error(error);
       }
     };
-  }, []);
-  console.log(currentBreed);
-  return data?.map((item, index) => {
-    setCurrentBreed(() => item);
-    return (
-      <div className="card" style={{ width: "18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{item}</h5>
+
+    fetchImageData();
+  }, [data]);
+
+  console.log(images);
+
+  return (
+    <div>
+      {data?.map((item, index) => (
+        <div className="card" style={{ width: "18rem" }} key={index}>
+          <img src={images[index]} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{item}</h5>
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+          <div className="card-body">
+            <a href="#" className="btn btn-primary">
+              Card link
+            </a>
+            <a href="#" className="btn btn-primary">
+              Another link
+            </a>
+          </div>
         </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-        </ul>
-        <div className="card-body">
-          <a href="#" className="btn btn-primary">
-            Card link
-          </a>
-          <a href="#" className="btn btn-primary">
-            Another link
-          </a>
-        </div>
-      </div>
-    );
-  });
+      ))}
+    </div>
+  );
 };
 
 export default DogDetail;
